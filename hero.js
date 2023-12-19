dbdef3ffc0a4c21e4fea87d3438335c9b7efc15e
Silo30 <silomenezes3010@gmail.com>
2023-12-18 21:47:28 -0300

    fim?

--- hero.js
+++ hero.js
@@ -1,12 +1,22 @@
-let hero = ["Dimitri", 1100]
+let hero = ["Dimitri", 100100]
 let rank
 
 if(hero[1] <= 1000){
     rank = "ferro"
 } else if(hero[1] <= 2000){
     rank = "bronze"
-} else {
+} else if(hero[1] <= 5000){
     rank = "prata"
+} else if(hero[1] <= 7000){
+    rank = "Ouro"
+} else if(hero[1] <= 8000){
+    rank = "Platina"
+} else if(hero[1] <= 9000){
+    rank = "Ascendente"
+} else if(hero[1] <= 10000){
+    rank = "Imortal"
+} else {
+    rank = "Radiante"
 }
 
-console.log(rank)
+console.log("O herói de nome " + hero[0] + " está no nível de " + rank)
